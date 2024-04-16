6a3f781237be11737b3517495726010c814f2a20
buesma <busching@kth.se>
2024-04-16 14:51:22 +0200

    Modified default optimization for the cloth splatting.

--- arguments/cloth_splatting/default.py
+++ arguments/cloth_splatting/default.py
@@ -1,10 +1,17 @@
 
-OptimizationParams=dict(
+OptimizationParams = dict(
     densification_interval=200,
     densify_from_iter=1000,
-    densify_until_iter=15_000,
+    densify_until_iter=20_000,
+    #
+    densify_grad_threshold_fine_init=0.001,
+    densify_grad_threshold_after=0.005,
+    #
     pruning_from_iter=1000,
     pruning_interval=200,
+    opacity_threshold_fine_init=0.001,
+    opacity_threshold_fine_after=0.005,
+
     initial_gaussians=1000,
-    no_coarse=True
+    no_coarse=True,
 )
