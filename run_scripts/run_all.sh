cd1608ef5aa38ffdd8c67ca9a7c20fdd48b7d379
buesma <busching@kth.se>
2024-10-16 14:07:26 +0200

    Update full run script.

--- run_scripts/run_all.sh
+++ run_scripts/run_all.sh
@@ -1,18 +1,18 @@
-export TEST_ITER=6000
+export TEST_ITER=8000
 #!/bin/bash
 
-#rm -r output/full_run
-mkdir -p output/full_run
+EXPNAME="full_run_rebuttal_upd"
+mkdir -p output/${EXPNAME}
 for SCENE in "TOWEL_00_03" "TSHIRT_01_00" "TSHIRT_01_01" "SHORTS_01_00" "SHORTS_01_01"
 do
-    EXP="full_run/${SCENE}"
+    EXP="${EXPNAME}/${SCENE}"
     DATA=data/folding_scenes/${SCENE}
-#    python3 train.py -s ${DATA} --expname "${EXP}" --configs arguments/cloth_splatting/default.py --view_skip 3 --iterations 6000
+    python3 train.py -s ${DATA} --expname "${EXP}" --configs arguments/cloth_splatting/default.py --view_skip 3 --iterations 6000
     python3 render_experimental.py -s ${DATA} --model_path "output/${EXP}" \
       --configs "arguments/cloth_splatting/default.py"  --meshnet_path "output/${EXP}/meshnet"\
       --show_flow --log_deform --track_vertices --skip_train --skip_video --iteration ${TEST_ITER} --flow_skip 2
     python3 scripts/align_eval_trajs.py  --gt_file "${DATA}/trajectory.npz" --traj_file "output/${EXP}/test/ours_${TEST_ITER}/all_trajs.npz"
-    mkdir -p output/full_run/tracking/${SCENE}
+    mkdir -p output/${EXPNAME}/tracking/${SCENE}
     python3 scripts/extract_aligned_trajs.py \
-    --src_dir "output/${EXP}" --target_dir output/full_run/tracking/${SCENE} --iteration ${TEST_ITER} --target_name cloth-splatting.npz
+    --src_dir "output/${EXP}" --target_dir output/${EXPNAME}/tracking/${SCENE} --iteration ${TEST_ITER} --target_name cloth-splatting.npz
 done
